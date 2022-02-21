fc71a60794eef990ddf02d8677d08d2638c1d18c
vinoths-tech <93162064+vinoths-tech@users.noreply.github.com>
2022-02-20 22:09:27 -0500

    Updated week4 workouts

---
+++ week4/green_taxi_data_web_to_gcs.py
@@ -0,0 +1,87 @@
+import os
+import pandas as pd
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+import pyarrow as pa
+from google.cloud import storage
+import logging
+
+"""
+Pre-reqs: 
+1. `pip install pandas pyarrow google-cloud-storage`
+2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
+3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
+"""
+
+# services = ['fhv','green','yellow']
+init_url = 'https://nyc-tlc.s3.amazonaws.com/trip+data/'
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc_data_lake_vs-dtc-de")
+
+table_schema = pa.schema(
+    [
+        ('VendorID',pa.string()),
+        ('lpep_pickup_datetime',pa.timestamp('s')),
+        ('lpep_dropoff_datetime',pa.timestamp('s')),
+        ('store_and_fwd_flag',pa.string()),
+        ('RatecodeID',pa.int64()),
+        ('PULocationID',pa.int64()),
+        ('DOLocationID',pa.int64()),
+        ('passenger_count',pa.int64()),
+        ('trip_distance',pa.float64()),
+        ('fare_amount',pa.float64()),
+        ('extra',pa.float64()),
+        ('mta_tax',pa.float64()),
+        ('tip_amount',pa.float64()),
+        ('tolls_amount',pa.float64()),
+        ('ehail_fee',pa.float64()),
+        ('improvement_surcharge',pa.float64()),
+        ('total_amount',pa.float64()),
+        ('payment_type',pa.int64()),
+        ('trip_type',pa.int64()),
+        ('congestion_surcharge',pa.float64()),
+    ]
+)
+
+def format_to_parquet(src_file):
+    if not src_file.endswith('.csv'):
+        logging.error("Can only accept source files in CSV format, for the moment")
+        return
+    table = pv.read_csv(src_file)
+    table = table.cast(table_schema)
+    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+
+
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    """
+    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+def web_to_gcs(year, service):
+    for i in range(13):
+        if i != 12:
+            month = '0'+str(i+1)
+            month = month[-2:]
+            file_name = service + '_tripdata_' + year + '-' + month + '.csv'
+            request_url = init_url + file_name
+            df = pd.read_csv(request_url)
+            df.to_csv(file_name, index = False)
+            print(f"Local: {file_name}")
+            parquetized = format_to_parquet(file_name)
+            file_name = file_name.replace('.csv', '.parquet')
+            print(f"Parquet: {file_name}")
+            upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
+            print(f"GCS: {service}/{file_name}")
+
+
+web_to_gcs('2019', 'green')
+web_to_gcs('2020', 'green')
